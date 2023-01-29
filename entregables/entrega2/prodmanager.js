dfcd75495ebc0b25fa1886d57a855386c4512ac7
whitewolf <santidotpy@gmail.com>
2023-01-29 03:23:37 -0300

    add updateProducts

--- entregables/entrega2/prodmanager.js
+++ entregables/entrega2/prodmanager.js
@@ -76,14 +76,42 @@ class ProductManager{
         }
     }
 
-    // updateProduct(code, title, description, price, thumbnail, stock){
-    //     if (this.products.find(item => item.code === code) != undefined) {
-    //         let prod =  this.products.find(item => item.code === code)
-    //         console.log(prod)
-    //     } else {
-    //         return 'Not Found'
-    //     }
-    // }
+    updateProduct(code, data){
+        const fs = require('fs');
+        let text = fs.readFileSync(this.path, "utf-8");
+        text = JSON.parse(text)
+        const objIndex = text.findIndex((obj => obj.id === code));
+
+        if (text.find(item => item.code === code) != undefined) {
+            let ex = text.find(item => item.code === code)
+            //ex.title = title
+            ex.title = data ? data.title : ex.title;
+            ex.description = data == undefined ? data.description : ex.description;
+            ex.price = data == undefined ? data.price : ex.price;
+            ex.thumbnail = data == undefined ? data.thumbnail : ex.thumbnail;
+            ex.stock = data == undefined ? data.stock : ex.stock;
+
+
+            //console.log(ex)
+            //text[objIndex] = ex
+            console.log(text)
+            fs.writeFile(this.path, JSON.stringify(text), 'utf-8', err => {
+                if (err) {
+                    console.error(err);
+                };
+                });
+
+
+        } else {
+            return 'Not Found'
+        }
+        // if (this.products.find(item => item.code === code) != undefined) {
+        //     let prod =  this.products.find(item => item.code === code)
+        //     console.log('caquerman')
+        // } else {
+        //     return 'Not Found'
+        // }
+    }
 
   }
 
@@ -94,8 +122,8 @@ class ProductManager{
     //testingThings.addProduct('coso', 'blabla', 100, 'blabla', 20)
     //testingThings.getProducts
     //testingThings.deleteProduct(1)
-    //testingThings.updateProduct(2)
+    testingThings.updateProduct(2, {title: "Lechuga"})
 
 
-    console.log(testingThings.getProductById(2))
+    //console.log(testingThings.getProductById(2))
   //console.log(testingThings.getProducts)
