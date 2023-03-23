126d4822609739419606048aebee5f18eb4ab3c1
santidotpy <santidotpy@gmail.com>
2023-03-23 03:22:20 -0300

    add more requests

--- entregables/pre_entregas/pre_entrega2/src/routes/carts.routes.js
+++ entregables/pre_entregas/pre_entrega2/src/routes/carts.routes.js
@@ -5,59 +5,125 @@ const managerCart = new CartMongo();
 
 const routerCart = Router();
 
-routerCart.get("/carts", (req, res) => {
-  res.render("index");
+// POST para crear un carrito
+routerCart.post("/cart", async (req, res) => {
+  try {
+    const respuesta = await managerCart.addElements();
+
+    return res.status(200).json(respuesta);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 });
 
-routerCart.get("/carts/:cid", async (req, res) => {
-  const page = req.query.page || 1;
-  const limit = req.query.limit || 8;
-  const sort = req.query.sort || "asc";
+// GET de todos los productos de un carrito en particular
+routerCart.get("/cart/:id", async (req, res) => {
+  try {
+    const productos = await managerCart.getProductsCart();
+
+    if (productos) {
+      return res.status(200).json(productos);
+    }
+
+    res.status(200).json({
+      message: "Productos no encontrados",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+// POST para agregar productos al carrito
+routerCart.post("/cart/:id", async (req, res) => {
+  const { id } = req.params;
+  const { id_prod, cant } = req.body;
+
+  try {
+    const product = await managerCart.addProductCart(id, id_prod, cant);
+    res.status(204).json(product);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+// DELETE para eliminar todos los productos del carrito
+routerCart.delete("/cart/:cid", async (req, res) => {
+    const cartId = req.params.cid;
+    const deletedCart = await managerCart.deleteProductsCart(cartId);
+    console.log(`${cartId} deleted`);
+    res.render("cart/cart", {
+      title: "Carts",
+      carts: JSON.stringify(deletedCart),
+    });
+  });
+
+// PUT para actualizar productos del carrito
+routerCart.put("/cart/:cid", async (req, res) => {
   const cartId = req.params.cid;
-  const carts = await managerCart.getElementById(cartId);
-  console.log(carts);
+  const cart = req.body;
+  const updatedCart = await managerCart.updateElement(cartId, cart);
+  console.log(updatedCart);
   res.render("cart/cart", {
     title: "Carts",
-    carts: carts.products,
-    // pagination: carts.pagination,
-    // currentPage: carts.page,
-    // totalPages: carts.totalPages,
+    carts: JSON.stringify(updatedCart),
   });
 });
 
-// agregar productos al carrito
-routerCart.post("/carts", async (req, res) => {
-    const cart = req.body; // array of products
-    console.log(cart);
-    const newCart = await Promise.all(
-        cart.map(async (cart) => {
-            const {
-                products,
-            } = cart;
-            const newCart = await managerCart.addElements({
-                products,
-            });
-            return newCart;
+// PUT para actualizar productos del carrito
+routerCart.put("/cart/:cid/product/:pid", async (req, res) => {
+    const { id } = req.params.cid;
+    const { id_prod } = req.params.pid;
+    const { quantity } = req.body;
+    try {
+        const product = await managerCart.updateProductCart(id, { id_prod, quantity })
+
+        if (product) {
+            return res.status(200).json({
+                message: "Producto actualizado"
+            })
+        }
+
+        res.status(200).json({
+            message: "Producto no encontrado"
         })
-    );
-    console.log(newCart);
-    res.render("cart/cart", {
-        title: "Carts",
-        carts: JSON.stringify(newCart),
-    });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+
 });
 
-// put para actualizar productos del carrito
-routerCart.put("/carts/:cid", async (req, res) => {
-    const cartId = req.params.cid;
-    const cart = req.body;
-    const updatedCart = await managerCart.updateElement(cartId, cart);
-    console.log(updatedCart);
-    res.render("cart/cart", {
-        title: "Carts",
-        carts: JSON.stringify(updatedCart),
-    });
-});
 
+
+// DELETE para eliminar productos del carrito
+routerCart.delete("/carts/product/:pid", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await managerCart.deleteProductCart(id);
+    console.log(product);
+
+    if (product) {
+      return res.status(200).json({
+        message: "Producto eliminado",
+      });
+    }
+
+    res.status(200).json({
+      message: "Producto no encontrado",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
 
 export default routerCart;
